fd35c4d23e2269d833930dd88dc2ea31001218c6
Krister Hedfors <kristerhedfors@users.noreply.github.com>
2023-05-28 22:22:53 +0200

    inline python function modification in progress, still buggy

--- ai.py
+++ ai.py
@@ -23,7 +23,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 COMMANDS = ['clear-state', 'show-state', 'use-state', 'list-state', 'save-state',
             'update-file', 'update-file-retry',
-            'summarize-file',
+            'summarize-file', 'explain-file',
             'set-system-role', 'get-system-role', 'debug', 'help', 'quit']
 
 
@@ -60,6 +60,24 @@ def display_diff(orig_string, updated_string):
     print(highlighted_text)
 
 
+def extract_python_function(filename, func_name):
+    # extract source code of function from filename
+    with open(filename, 'r') as f:
+        content = f.read()
+    # find the function definition
+    func_def = 'def {}'.format(func_name)
+    func_start = content.find(func_def)
+    if func_start == -1:
+        raise Exception(f"Could not find function definition for {func_name}")
+    # find the end of the function
+    func_end = content.find('\n\n', func_start)
+    if func_end == -1:
+        raise Exception(f"Could not find end of function definition for {func_name}")
+    # extract the function
+    func = content[func_start:func_end]
+    return func, func_def, func_start, func_end
+
+    
 def complete(text, state):
     for cmd in COMMANDS:
         if cmd.startswith(text):
@@ -130,7 +148,54 @@ def load_model():
     else:
         return "gpt-3.5-turbo"
 
+def extract_single_block_of_python_code_from_GPT_answer(answer):
+    code_block_delimiter = '```'
+    #
+    # sometimes code block delimiter is missing
+    #
+    if not code_block_delimiter in answer:
+        return answer
+    if answer.count(code_block_delimiter) != 2:
+        raise Exception("Expected 0 or 2 code block delimiters, found", answer.count(code_block_delimiter))
+    code_block_start = answer.find(code_block_delimiter)
+    if code_block_start == -1:
+        return answer
+    code_block_end = answer.find(code_block_delimiter, code_block_start + len(code_block_delimiter))
+    if code_block_end == -1:
+        raise Exception("Could not find code block end")
+    code_block = answer[code_block_start + len(code_block_delimiter):code_block_end]
+    return code_block
+
+
 def update_file(filename, instruction):
+    func_name = func_def = func_start = func_end = None
+    is_python_file = False
+    if ':' in filename:
+        filename, func_name = filename.split(':')
+        #
+        # Only Python support atm.
+        #
+        if filename.endswith('.py'):
+            is_python_file = True
+        assert is_python_file
+        content, func_def, func_start, func_end = extract_python_function(filename, func_name)
+        #content = content.replace(f'def {func_name}', 'def func')
+        msg = f"Update the function {func_name} in {filename}, file contents pasted below the two newlines. "
+        msg += f"Return nothing but the updated function definition, "
+        msg += f"commented, according the following instruction: {instruction}"
+        msg += '\n\n'
+        msg += content
+    else:
+        if filename.endswith('.py'):
+            is_python_file = True
+        with open(filename, 'r') as f:
+            content = f.read()
+        msg = f"Update the file {filename}, file contents pasted below the two newlines. "
+        msg += f"Return nothing but the updated file contents, "
+        msg += f"commented, according the following instruction: {instruction}"
+        msg += '\n\n'
+        msg += content
+
     lexer = None
     if filename.endswith('.py'):
         lexer = PythonLexer()
@@ -143,13 +208,6 @@ def update_file(filename, instruction):
     if filename.endswith('.sh') or filename.endswith('.bash'):
         lexer = BashLexer()
 
-    with open(filename, 'r') as f:
-        content = f.read()
-    msg = f"Update the file {filename}, file contents pasted below the two newlines. "
-    msg += f"Return nothing but the updated file contents, "
-    msg += f"commented, according the following instruction: {instruction}"
-    msg += '\n\n'
-    msg += content
 
     answer = ask_gpt3(msg)
     #
@@ -158,6 +216,16 @@ def update_file(filename, instruction):
     hl_answer = answer
     if not lexer is None:
         hl_answer = highlight(answer, lexer, TerminalFormatter())
+    
+    if is_python_file:
+        #
+        # extract function definition from answer
+        #
+        answer = extract_single_block_of_python_code_from_GPT_answer(answer)
+        #
+        # Keep entire response in highligted response in interactive mode
+        #
+        #hl_answer = highlight(answer, PythonLexer(), TerminalFormatter())
 
     print(f"{hl_answer}")
 
@@ -166,6 +234,17 @@ def update_file(filename, instruction):
         if accept.lower() == 'diff':
             display_diff(content, answer)
         elif accept.lower() == 'show':
+            print(f"{hl_answer}")
+        elif accept.lower() not in ('y', 'n'):
+            instruction = accept
+            answer = ask_gpt3(msg)
+            #
+            # syntax highlighting
+            #
+            hl_answer = answer
+            if not lexer is None:
+                hl_answer = highlight(answer, lexer, TerminalFormatter())
+
             print(f"{hl_answer}")
         else:
             break
@@ -177,15 +256,39 @@ def update_file(filename, instruction):
         backup_filename = filename + '.bak.' + str(int(os.path.getmtime(filename)))
         with open(backup_filename, 'w') as f:
             f.write(content)
-        #
-        # update file
-        #
-        with open(filename, 'w') as f:
-            f.write(answer)
+
+        if not func_name is None:
+            #
+            # replace function definition
+            #
+            with open(filename, 'w') as f:
+                f.write(content[:func_start])
+                f.write(answer)
+                f.write(content[func_end:])
+        else:
+            #
+            # update file
+            #
+            with open(filename, 'w') as f:
+                f.write(answer)
+        
+        print("Update accepted. Backup saved to", backup_filename)
+
     elif accept.lower() == 'n':
         print("Update rejected.")
+
     else:
-        pass
+        raise Exception('should not happen')
+
+
+def explain_file(filename):
+    with open(filename, 'r') as f:
+        content = f.read()
+    msg = f"Explain the file {filename}, file contents pasted below the two newlines. "
+    msg += '\n\n'
+    msg += content
+    answer = ask_gpt3(msg)
+    print(f"Explanation:\n\n{answer}")
 
 
 def summarize_file(filename):
@@ -302,6 +405,9 @@ def main():
                 elif question.lower().startswith('summarize-file'):
                     filename = question.split(' ', 1)[1]
                     summarize_file(filename)
+                elif question.lower().startswith('explain-file'):
+                    filename = question.split(' ', 1)[1]
+                    explain_file(filename)
                 
                 #
                 # system functions
